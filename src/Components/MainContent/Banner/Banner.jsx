d59df2317f54fc01de74504090adadce5c878647
Saif-Rahman-801 <saifurs102@gmail.com>
2023-12-11 22:42:22 +0600

    experience section designed

--- src/Components/MainContent/Banner/Banner.jsx
+++ src/Components/MainContent/Banner/Banner.jsx
@@ -1,6 +1,5 @@
 import { MdOutlineFileDownload } from "react-icons/md";
 import { IoMdContact } from "react-icons/io";
-// import TypeAnimation from "react-type-animation";
 import profile from "../../../assets/saif-removebg-preview.png";
 import JS from "../../../assets/js.png";
 import react from "../../../assets/atom.png";
@@ -10,7 +9,7 @@ import mongoDb from "../../../assets/icons8-mongo-db-64.png";
 import { TypeAnimation } from "react-type-animation";
 
 const Banner = () => {
-  const filePath = "/public/resume1.pdf";
+  const filePath = "../../../../public/resume1.pdf";
 
   return (
     <div className="flex flex-col md:flex-row justify-center items-center gap-5">
